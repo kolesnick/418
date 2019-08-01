4bc3220e8f20f4222757df017a3081a982a7e137
Anatoliy Kolesnick <anatoliy.kolesnick@binibambini.com>
2019-08-01 17:43:15 +0300

    included timeout to entity string

--- src/I'm a Teapot/Assets/GameEntity.cs
+++ src/I'm a Teapot/Assets/GameEntity.cs
@@ -1,7 +1,15 @@
-﻿public sealed partial class GameEntity
+﻿using System.Linq;
+
+public sealed partial class GameEntity
 {
     public override string ToString() =>
         isEmployee
-            ? $"Employee :: {base.ToString()}"
+            ? $"{nameof(Employee)} [ {FormatComponentsExcept(nameof(Employee))} ]"
             : base.ToString();
+
+    private string FormatComponentsExcept(string component) =>
+        GetComponents()
+            .Select(x => x.ToString())
+            .Except(new [] { component })
+            .Aggregate((x, y) => $"{x}, {y}");
 }
