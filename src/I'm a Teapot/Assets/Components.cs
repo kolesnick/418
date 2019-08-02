0955340e6e38cbd1224ccddaa5d75fe7262a5dcc
Anatoliy Kolesnick <anatoliy.kolesnick@binibambini.com>
2019-08-02 08:31:32 +0300

    timeout string now does no include timeout word

--- src/I'm a Teapot/Assets/Components.cs
+++ src/I'm a Teapot/Assets/Components.cs
@@ -3,7 +3,7 @@
 // ReSharper disable CheckNamespace
 // ReSharper disable InconsistentNaming
 
-public class Timeout : IComponent { public float value; public override string ToString() => $"{nameof(Timeout)}: {value:F1}"; }
+public class Timeout : IComponent { public float value; public override string ToString() => $"{value:F1}"; }
 
 public class Employee : IComponent { }
 public class Thirsty : IComponent { }
